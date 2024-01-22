473cd1edb971973945af804de3fbc21de4561833
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-22 02:02:07 -0500

    udpated

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,19 +1,23 @@
-// Creating token and saving in cookie
 const sendToken = (user, statusCode, res, msg) => {
   const token = user.getJWTToken();
 
-  // options for cookies
+  // Options for cookies
   const options = {
     expires: new Date(
       Date.now() + parseInt(process.env.COOKIE_EXPIRE, 10) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Only send cookies over HTTPS
+    sameSite: "None", // if your frontend and backend are served from different origins
   };
+
+  // Omit sensitive data
+  const { password, ...userDetails } = user.toObject();
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     message: msg,
-    user,
+    user: userDetails,
     token,
   });
 };
